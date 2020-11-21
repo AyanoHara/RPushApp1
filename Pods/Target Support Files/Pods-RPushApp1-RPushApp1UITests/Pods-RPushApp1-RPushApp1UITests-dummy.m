4aee3b8c20b31ebcc9f3a54b01714e639bd2875b
Ayano Hara <ayano0127@gmail.com>
2020-11-21 16:01:22 +0900

    初期設定

---
+++ Pods/Target Support Files/Pods-RPushApp1-RPushApp1UITests/Pods-RPushApp1-RPushApp1UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RPushApp1_RPushApp1UITests : NSObject
+@end
+@implementation PodsDummy_Pods_RPushApp1_RPushApp1UITests
+@end
---
+++ Pods/Target Support Files/Pods-RPushApp1/Pods-RPushApp1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RPushApp1 : NSObject
+@end
+@implementation PodsDummy_Pods_RPushApp1
+@end
